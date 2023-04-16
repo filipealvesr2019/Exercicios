e83cf8b24c3a73e755c3b02b4938c32b93efbae5
Filipe Alves <filipealvesrds2019@gmail.com>
2023-04-16 19:56:35 -0300

    cm

--- Node_js/day 5/index.js
+++ Node_js/day 5/index.js
@@ -1,1 +1,7 @@
-function 
+function direita(direita){
+    document.getElementById("direita").style.visibility = 'visible';
+}
+
+function esquerda(esquerda){
+    document.getElementById("esquerda").style.visibility = 'visible';
+}
