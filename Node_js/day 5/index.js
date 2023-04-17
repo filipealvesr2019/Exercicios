b931fa7dc8dd7075086fbee763c6bf00be6be715
Filipe Alves <filipealvesrds2019@gmail.com>
2023-04-17 00:09:56 -0300

    cm

--- Node_js/day 5/index.js
+++ Node_js/day 5/index.js
@@ -1,7 +1,49 @@
-function direita(direita){
-    document.getElementById("direita").style.visibility = 'visible';
+function direita(image1){
+    document.getElementById("image1");
 }
 
-function esquerda(esquerda){
-    document.getElementById("esquerda").style.visibility = 'visible';
+function esquerda(image){
+    document.getElementById("image");
 }
+
+function addE(cardI){
+    var esquerda = document.getElementById("image1");
+    var Element = document.querySelector(cardI);
+    Element.classList.toggle("direita")
+}
+
+function addEL(cardID){
+    var esquerda = document.getElementById("image");
+    var Element = document.querySelector(cardID);
+    Element.classList.toggle("esquerda")
+}
+
+function checkKeyBoardCode()
+{
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var name = event.code;
+
+        alert(`tecla pressionada ${name} \r\n valor do keycode: ${code}`);
+    }, false);
+}
+function addChange(){
+    document.addChange('keydown', (event) => {
+        var imagem1 = getElementById("image1");
+        var imagem2 = getElementById("image")
+
+        var code = event.code;
+        if(code == 'digite1'){
+            imagem1.classList.toggle('a').style.visibility = "invisible";
+            imagem2.classList.remove('b').style.visibility = "visible";
+        }
+        if(code == 'digite2'){
+            imagem1.classList.toggle('a').style.visibility = "visible";
+            imagem2.classList.toggle('b').style.visibility = "visible";
+        }
+    },false);
+    console.log(addChange());
+}
+
+checkKeyBoardCode();
+console.log(checkKeyBoardCode());
