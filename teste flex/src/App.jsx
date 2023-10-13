7c64058a44b4761ad718074a173120f36388dc9c
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-13 19:33:49 -0300

    commite

--- teste flex/src/App.jsx
+++ teste flex/src/App.jsx
@@ -43,20 +43,7 @@ function App() {
         
       </div>
 
-
-      <div className="navc">
-        <nav className="nav_listc">
-          <div className="logoc">logo</div>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Sing in</a></li>
-        </nav>
-        
-      </div>
+      
 
       <div className="clamp">
         <div className="left">
