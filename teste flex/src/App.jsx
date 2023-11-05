ab15a0b45f1614d41206926fbeac67546a96f532
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-04 23:58:51 -0300

    cm

--- teste flex/src/App.jsx
+++ teste flex/src/App.jsx
@@ -1,31 +1,103 @@
-import Modal from "./modal"
+import { useState } from "react";
 import "./App.css";
 import MobileMenu from "./MobileMenu";
 
 function App() {
   const items = Array.from(Array(100).keys()); // Criar um array de 100 itens
+  const [selectedItem, setSelectedItem] = useState('Home');
 
+  const handleItemClick = (itemName) => {
+    setSelectedItem(itemName);
+  };
 
+  const getIcon = (itemName) => {
+    if (selectedItem === itemName) {
+      switch (itemName) {
+        case 'Home':
+          return 'https://i.postimg.cc/PfgLCRKR/home-1.png';
+        case 'Produtos':
+          return 'https://i.postimg.cc/RC762zcL/box-3.png';
+        case 'Pedidos':
+          return 'https://i.postimg.cc/vBQFB9NB/to-do-list-1.png';
+        case 'Configurações':
+          return 'https://i.postimg.cc/DwPKKBxD/setting-1.png';
+        default:
+          return '';
+      }
+    } else {
+      switch (itemName) {
+        case 'Home':
+          return 'https://i.postimg.cc/zD4FjpDs/home.png';
+        case 'Produtos':
+          return 'https://i.postimg.cc/nr9Yx92S/box.png';
+        case 'Pedidos':
+          return 'https://i.postimg.cc/G2gwNVgJ/to-do-list.png';
+        case 'Configurações':
+          return 'https://i.postimg.cc/3xZK1Sh9/setting.png';
+        default:
+          return '';
+      }
+    }
+  };
 
   return (
     <>
-     <MobileMenu></MobileMenu>
-        <div className="grid-container">
-      {items.map((item) => (
-        <div key={item} className="grid-item"></div>
-      ))}
-    </div>
-     
+      <MobileMenu></MobileMenu>
+      <div className="grid-container">
+        {items.map((item) => (
+          <div key={item} className="grid-item"></div>
+        ))}
+      </div>
+
       <footer className="footer">
         <nav>
-          <a href="#"><img src="https://i.postimg.cc/zD4FjpDs/home.png" alt="" /><span>Home</span> </a>
-          <a href="#"><img src="https://i.postimg.cc/nr9Yx92S/box.png" alt="" /><span>Produtos</span> </a>
-          <a href="#"><img src="https://i.postimg.cc/G2gwNVgJ/to-do-list.png"></img><span>Pedidos</span></a>
-          <a href="#"><img src="https://i.postimg.cc/3xZK1Sh9/setting.png"></img><span>Configurações</span></a>
+          <a
+            
+            className={selectedItem === 'Home' ? 'selected' : ''}
+            onClick={() => handleItemClick('Home')}
+          >
+            <img
+              src={getIcon('Home')}
+              alt=""
+            />
+            <span>Home</span>
+          </a>
+          <a
+           
+            className={selectedItem === 'Produtos' ? 'selected' : ''}
+            onClick={() => handleItemClick('Produtos')}
+          >
+            <img
+              src={getIcon('Produtos')}
+              alt=""
+            />
+            <span>Produtos</span>
+          </a>
+          <a
+            
+            className={selectedItem === 'Pedidos' ? 'selected' : ''}
+            onClick={() => handleItemClick('Pedidos')}
+          >
+            <img
+              src={getIcon('Pedidos')}
+              alt=""
+            />
+            <span>Pedidos</span>
+          </a>
+          <a
+           
+            className={selectedItem === 'Configurações' ? 'selected' : ''}
+            onClick={() => handleItemClick('Configurações')}
+          >
+            <img
+              src={getIcon('Configurações')}
+              alt=""
+            />
+            <span>Configurações</span>
+          </a>
         </nav>
       </footer>
-
-          </>
+    </>
   );
 }
 
