8818063f9d059fa010e93d0cd7e6350d7cf0a2a2
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-04 22:43:41 -0300

    cm

--- teste flex/src/App.jsx
+++ teste flex/src/App.jsx
@@ -3,14 +3,27 @@ import "./App.css";
 import MobileMenu from "./MobileMenu";
 
 function App() {
+  const items = Array.from(Array(100).keys()); // Criar um array de 100 itens
 
 
 
   return (
     <>
+     <MobileMenu></MobileMenu>
+        <div className="grid-container">
+      {items.map((item) => (
+        <div key={item} className="grid-item"></div>
+      ))}
+    </div>
      
-      <MobileMenu></MobileMenu>
-    
+      <footer className="footer">
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Produtos</a>
+          <a href="#">Pedidos</a>
+          <a href="#">Configurações</a>
+        </nav>
+      </footer>
 
           </>
   );
