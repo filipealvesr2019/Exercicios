d1aa75eb2747085342c140828b89a522b5eed404
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-08 10:59:40 -0300

    cm

--- teste flex/src/MobileMenu.jsx
+++ teste flex/src/MobileMenu.jsx
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 const MobileMenu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
-
+  const [backgroundColor, setBackgroundColor] = useState('blue');
+  const items = [
+    { id: 'img', text: 'Pedidos', icon: 'https://i.ibb.co/LhhvcB0/box.png' },
+    // Adicione mais itens conforme necessário
+  ];
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -23,23 +27,27 @@ const MobileMenu = () => {
           <img
             src="https://i.postimg.cc/gk2pt3N8/close.png"
             alt="Menu Icon"
-            style={{ width: '30px', height: '30px' , marginLeft:"-1rem"}} // ajuste o tamanho conforme necessário
+            style={{ width: '30px', height: '30px' }} // ajuste o tamanho conforme necessário
           />
         ) : (
           <img
             src="https://i.postimg.cc/g2jVNtCR/menu.png"
             alt="Menu Icon"
-            style={{ width: '30px', height: '30px', marginLeft:"-1rem" }} // ajuste o tamanho conforme necessário
+            style={{ width: '30px', height: '30px' }}
+            // ajuste o tamanho conforme necessário
           />
         )}
       </div>
       {isMenuOpen && (
-        <div className="menu-content" style={{ marginTop: '20px' }}>
+        <div
+          className="menu-content"
+          style={{ marginTop: '20px', backgroundColor: '#14337D', width: '500px', padding: '2rem', fontFamily: 'urbanist', fontWeight: '600', fontSize: '2rem' }}
+        >
           <ul style={{ listStyleType: 'none', padding: 0 }}>
             <li
               style={{
                 margin: '10px 0',
-                color: selectedItem === 'Item 1' ? 'black' : 'gray',
+                color: selectedItem === 'Item 1' ? '#4FD6F7' : 'white',
               }}
               onClick={() => handleItemClick('Item 1')}
             >
@@ -48,7 +56,7 @@ const MobileMenu = () => {
             <li
               style={{
                 margin: '10px 0',
-                color: selectedItem === 'Item 2' ? 'black' : 'gray',
+                color: selectedItem === 'Item 2' ? 'gray' : 'white',
               }}
               onClick={() => handleItemClick('Item 2')}
             >
@@ -57,13 +65,12 @@ const MobileMenu = () => {
             <li
               style={{
                 margin: '10px 0',
-                color: selectedItem === 'Item 3' ? 'black' : 'gray',
+                color: selectedItem === 'Item 3' ? 'gray' : 'white',
               }}
               onClick={() => handleItemClick('Item 3')}
             >
               Pedidos
             </li>
-            {/* Adicione mais itens conforme necessário */}
           </ul>
         </div>
       )}
